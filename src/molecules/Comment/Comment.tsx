ba4a0f181ff41230046468fa8aa1c2637a14afe3
ぶち <6uclz1@users.noreply.github.com>
2023-04-16 19:51:38 +0900

    ダミー関数を作成

--- src/molecules/Comment/Comment.tsx
+++ src/molecules/Comment/Comment.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./../App.css";
-import Icon from "../atoms/Icon/Icon";
-import Message from "../atoms/Message/Message";
+import Icon from "../../atoms/Icon/Icon";
+import Message from "../../atoms/Message/Message";
+import "./Comment.css";
 
-const style2: React.CSSProperties = {
+const style: React.CSSProperties = {
   display: "flex",
   backdropFilter: "blur(5px)",
   margin: "8px",
@@ -13,7 +13,7 @@ const style2: React.CSSProperties = {
 function ChatMessage(props: any) {
   const { text } = props;
   return (
-    <div className={"animate"} style={style2}>
+    <div className={"chat-message"} style={style}>
       <Icon />
       <Message text={text} />
     </div>
---
+++ src/organisms/CharactorMessageWindow/CharactorMessageWindow.tsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import AnimatedText from "../../atoms/Message/AnimatedText";
+import { generateDummyText } from "../../services/CharactorMessage/CharactorMessage";
+
+const CharactorMessageWindow: any = () => {
+  const [response, setResponse]: any[] = useState("");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // TODO: APIを使用した取得方法に変更する
+      const responseText = generateDummyText;
+      setResponse(responseText);
+    }, 10000);
+    return () => clearInterval(intervalId);
+  }, [response]);
+
+  return <AnimatedText text={response} speed={10} />;
+};
+
+export default CharactorMessageWindow;
