79e269d23c6cba4ec1d088fbb35caf044ccbf196
ぶち <6uclz1@users.noreply.github.com>
2023-05-01 00:26:32 +0900

    [WIP] Charactor Animation.

--- src/atoms/Charactor/Charactor.tsx
+++ src/atoms/Charactor/Charactor.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import charactor from "./charactor.png";
+import charactor2 from "./charactor2.png";
 import "../../App.css";
 
 const style: React.CSSProperties = {
@@ -11,7 +12,23 @@ const style: React.CSSProperties = {
 };
 
 function Charactor() {
-  return <img style={style} src={charactor} alt="" />;
+  const images = [
+    { src: charactor, displayTime: 6000 },
+    { src: charactor2, displayTime: 400 },
+  ];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((currentImageIndex + 1) % images.length);
+    }, images[currentImageIndex].displayTime*Math.random());
+
+    return () => clearInterval(interval);
+  }, [currentImageIndex]);
+
+  return (
+    <img style={style} src={images[currentImageIndex].src} alt="animated image" />
+  );
 }
 
 export default Charactor;
